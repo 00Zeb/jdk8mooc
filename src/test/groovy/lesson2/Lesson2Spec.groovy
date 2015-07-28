f83687607bf56eb84d400affec27b44889083992
Sebastian Larsson <sebastian.larsson@cybercom.com>
2015-07-28 10:06:21 +0200

    added test for exercise 7

--- src/test/groovy/lesson2/Lesson2Spec.groovy
+++ src/test/groovy/lesson2/Lesson2Spec.groovy
@@ -93,4 +93,15 @@ class Lesson2Spec extends Specification {
 		then: "All words in the file are printed to the console in lowercase and only once, sorted in natural order."
 		1 * console.println(['a', 'abundance', 'and', 'and', 'art', 'as', 'be', 'bear', 'beauty\'s', 'bright', 'bud', 'buriest', 'but', 'by', 'churl', 'content', 'contracted', 'creatures', 'cruel', 'decease', 'desire', 'die', 'due', 'eat', 'else', 'eyes', 'fairest', 'famine', 'feed\'st', 'flame', 'foe', 'fresh', 'from', 'fuel', 'gaudy', 'glutton', 'grave', 'heir', 'herald', 'his', 'his', 'in', 'increase', 'lies', 'light\'s', 'mak\'st', 'making', 'memory', 'might', 'never', 'niggarding', 'now', 'only', 'or', 'ornament', 'own', 'pity', 'riper', 'rose', 'self', 'should', 'spring', 'substantial', 'sweet', 'tender', 'that', 'that', 'the', 'thee', 'thereby', 'thine', 'this', 'thou', 'thou', 'thy', 'thy', 'time', 'to', 'to', 'too', 'waste', 'we', 'where', 'with', 'within', 'world', 'world\'s'])
 	}
+
+	def "exercise 7"() {
+		given: "A console"
+		Console console = Mock()
+		when: "Parsing the file contents"
+		new Lesson2(console).exercise7()
+		then: "All words in the file are printed to the console in lowercase and only once, but sorted in length of the strings."
+		1 * console.println("something")
+	}
+
+
 }
