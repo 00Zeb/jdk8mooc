3642e56c30b5db79269a5817180ea1f0953f5e49
Sebastian Larsson <sebastian.larsson@cybercom.com>
2015-07-31 17:49:01 +0200

    added test for creating a randow word list.

--- src/test/groovy/lesson3/RandomWordsSpec.groovy
+++ src/test/groovy/lesson3/RandomWordsSpec.groovy
@@ -15,4 +15,14 @@ class RandomWordsSpec extends Specification {
 		then: "The words from the file are read into memory"
 		randomWords.allWords().size() == 63254
 	}
+	
+	def "Can get a list of randomized words"() {
+		given: "A file containing words"
+		Path path = Paths.get("words")
+		when: "Creating a random list with three elements."
+		RandomWords randomWordGenerator = new RandomWords(path);
+		List<String> randomWords = randomWordGenerator.createList(3)
+		then: "There should be three elements in the list."
+		randomWords.size() == 3
+	}
 }
