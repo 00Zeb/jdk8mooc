5b408a40360c119d0d5b7cdeebf1774aa44c16e4
Sebastian Larsson <sebastian.larsson@cybercom.com>
2015-08-06 19:29:08 +0200

    works

--- src/test/groovy/testing/ExampleSpec.groovy
+++ src/test/groovy/testing/ExampleSpec.groovy
@@ -4,29 +4,30 @@ import spock.lang.Specification;
 
 class ExampleSpec extends Specification {
 	
-	private static class Foo {
+	private final static class Foo {
 		Bar bar = new Bar();
 		
 		public List<String> getResult() {
-			return bar.getResult();
+			return Bar.getResult();
 		}
 	}
 	
 	private static class Bar {
 		
-		public List<String> getResult() {
+		public final static List<String> getResult() {
 			throw new RuntimeException();
 		}
 	}
 
 	def "my test"() {
 		given: 
-		Bar bar = Stub(Bar)
+		Bar bar = GroovyStub(Bar)
+		Bar.getResult() >> ["hej", "hopp"]
 		Foo foo = new Foo();
 		foo.bar = bar
 		when:
 		List<String> result = foo.getResult();
 		then:
-		result == []
+		result == ["hej", "hopp"]
 	}
 }
